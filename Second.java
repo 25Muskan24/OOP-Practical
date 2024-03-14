a66b48103c34cbef66e8d3935bd6f08ae4bb3414
25Muskan24 <choudharimuskan250204@gmail.com>
2024-03-14 15:09:52 +0530

    Practical 6 completed

---
+++ Second.java
@@ -0,0 +1,14 @@
+/* Aim:-PracticalNo6
+   Author:-Muskan Choudhary
+   Version:-6.0
+   Date:-5/03/24
+*/
+package Muskan;
+import learnjava.First;
+
+public class Second {
+    public static void main(String[] args) {
+        First firstObject = new First();
+        firstObject.welcomeMessage();
+    }
+} 
