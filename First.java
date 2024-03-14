78fc86c4ebf9d1ac1285d8d0a31de0b618676c2e
25Muskan24 <choudharimuskan250204@gmail.com>
2024-03-14 15:09:29 +0530

    Practical 6 completed

---
+++ First.java
@@ -0,0 +1,13 @@
+/* Aim:-PracticalNo6
+   Author:-Muskan Choudhary
+   Version:-6.0
+   Date:-5/03/24
+*/
+package learnjava;
+
+public class First {
+    public void welcomeMessage() {
+        System.out.println("Welcome to package");
+    }
+}
+
